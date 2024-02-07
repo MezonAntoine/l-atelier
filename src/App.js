d5250f170c31acaae9e7f9e44e10cd8c5b8f57d9
AntoineAllowa <antoine.mezon@allowa.com>
2024-02-07 11:24:10 +0100

    feat(App) add app Routes & delete App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import DuelPage from "./pages/duel/duel";
+import ResultPage from "./pages/ranking/ranking";
+import NotFound from "./pages/404/404";
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<DuelPage />} />
+        <Route path="/ranking" element={<ResultPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
